a01b674f0a745d16e1a75d5077fd29c823d274db
kirillchernik8 <kirillchernik8@icloud.com>
2019-01-14 15:56:26 -0500

    create Crub class and subclasses

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,11 @@
 class Grub {
+    constructor() {
+        this.age =0;
+        this.color = "pink";
+        this.food = "jelly";
+    }
+    eat () {return "Mmmmmmmmm jelly"}
 };
+
+let grub = new Grub()
+console.log(grub.age)
